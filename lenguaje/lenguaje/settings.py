6f8e363443fcffd4c74cf44c3c536855f257464a
Yeni1289 <154487288+Yeni1289@users.noreply.github.com>
2025-11-03 12:39:17 -0500

    Update settings.py

--- lenguaje/lenguaje/settings.py
+++ lenguaje/lenguaje/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'lenguaje.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
         'default': dj_database_url.config(default='sqlite:///db.sqlite3')
     }
 }

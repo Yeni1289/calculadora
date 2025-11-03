bcbac28cc3bd4e13fc88b4241d19b15f1d90eb4e
Yeni1289 <154487288+Yeni1289@users.noreply.github.com>
2025-11-03 12:52:41 -0500

    Update settings.py

--- lenguaje/lenguaje/settings.py
+++ lenguaje/lenguaje/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #'sslserver',
-    'lenguaje_app',  # <-- agregar la app aquí
+    #'lenguaje_app',  # <-- agregar la app aquí
+    'calculadora',
 ]
 
 
